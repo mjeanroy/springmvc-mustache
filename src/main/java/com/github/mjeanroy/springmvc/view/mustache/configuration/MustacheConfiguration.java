023f773c7e1d189cffd9505f62b8f0257e88cf7f
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2014-09-14 18:16:11 +0200

    styles: improve documentation of mustache configuration

--- src/main/java/com/github/mjeanroy/springmvc/view/mustache/configuration/MustacheConfiguration.java
+++ src/main/java/com/github/mjeanroy/springmvc/view/mustache/configuration/MustacheConfiguration.java
@@ -24,23 +24,22 @@
 
 package com.github.mjeanroy.springmvc.view.mustache.configuration;
 
-import static com.samskivert.mustache.Mustache.Compiler;
-import static java.lang.Boolean.parseBoolean;
-import static java.lang.Integer.parseInt;
-import static java.lang.String.valueOf;
-
+import com.github.mjeanroy.springmvc.view.mustache.MustacheCompiler;
 import com.github.mjeanroy.springmvc.view.mustache.MustacheTemplateLoader;
+import com.github.mjeanroy.springmvc.view.mustache.MustacheViewResolver;
 import com.github.mjeanroy.springmvc.view.mustache.core.DefaultMustacheTemplateLoader;
+import com.github.mjeanroy.springmvc.view.mustache.jmustache.JMustacheCompiler;
+import com.samskivert.mustache.Mustache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
-import com.github.mjeanroy.springmvc.view.mustache.MustacheCompiler;
-import com.github.mjeanroy.springmvc.view.mustache.MustacheViewResolver;
-import com.github.mjeanroy.springmvc.view.mustache.jmustache.JMustacheCompiler;
-import com.samskivert.mustache.Mustache;
+import static com.samskivert.mustache.Mustache.Compiler;
+import static java.lang.Boolean.parseBoolean;
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
 
 @Configuration
 @PropertySource(
@@ -49,17 +48,52 @@ import com.samskivert.mustache.Mustache;
 )
 public class MustacheConfiguration {
 
+	/**
+	 * Default prefix prepended to view names.
+	 * This prefix will be used by view resolver and mustache template
+	 * loader.
+	 */
 	public static final String PREFIX = "/templates/";
 
+	/**
+	 * Default suffix appended to view names.
+	 * This suffix will be used by view resolver and mustache template
+	 * loader.
+	 */
 	public static final String SUFFIX = ".template.html";
 
+	/**
+	 * Default view resolver order defined
+	 * on {@link org.springframework.web.servlet.view.UrlBasedViewResolver#setOrder}.
+	 * This settings set the order in which this {@link com.github.mjeanroy.springmvc.view.mustache.MustacheViewResolver}
+	 * will be evaluated.
+	 */
 	public static final int ORDER = 1;
 
-	public static final boolean CACHE = false;
+	/**
+	 * Default cache settings defined
+	 * on {@link org.springframework.web.servlet.view.UrlBasedViewResolver#setCache}.
+	 */
+	public static final boolean CACHE = true;
+
+	/**
+	 * Default cache settings defined
+	 * on {@link org.springframework.web.servlet.view.UrlBasedViewResolver#setCache}.
+	 * This settings set the view names (or name patterns) that can be handled by
+	 * {@link com.github.mjeanroy.springmvc.view.mustache.MustacheViewResolver}
+	 */
+	public static final String VIEW_NAMES = "*.template.html,*.mustache";
 
 	@Autowired
 	private Environment environment;
 
+	/**
+	 * Build mustache view resolver.
+	 * This view resolver needs an instance of {@link com.github.mjeanroy.springmvc.view.mustache.MustacheCompiler}
+	 * to be created.
+	 *
+	 * @return Mustache view resolver instance.
+	 */
 	@Bean
 	public MustacheViewResolver mustacheViewResolver() {
 		MustacheCompiler compiler = mustacheCompiler();
@@ -69,20 +103,24 @@ public class MustacheConfiguration {
 		String suffix = getSuffix();
 		int order = getOrder();
 		boolean cache = getCache();
+		String[] viewNames = getViewNames();
 
 		MustacheViewResolver resolver = new MustacheViewResolver(compiler);
 		resolver.setCache(cache);
 		resolver.setPrefix(prefix);
 		resolver.setSuffix(suffix);
 		resolver.setOrder(order);
-		resolver.setViewNames(new String[]{
-				"*.template.html",
-				"*.mustache"
-		});
-
+		resolver.setViewNames(viewNames);
 		return resolver;
 	}
 
+	/**
+	 * Build mustache compiler.
+	 * This compiler use an instance of {@link com.samskivert.mustache.Mustache.Compiler}
+	 * under the hood.
+	 *
+	 * @return Mustache compiler implementation.
+	 */
 	@Bean
 	public MustacheCompiler mustacheCompiler() {
 		MustacheTemplateLoader templateLoader = mustacheTemplateLoader();
@@ -96,24 +134,75 @@ public class MustacheConfiguration {
 		return new JMustacheCompiler(jmustacheCompiler, templateLoader);
 	}
 
+	/**
+	 * Build mustache template loader.
+	 * This compiler use an instance of {@link org.springframework.core.io.DefaultResourceLoader}
+	 * under the hood.
+	 *
+	 * @return Mustache template loader implementation.
+	 */
 	@Bean
 	public MustacheTemplateLoader mustacheTemplateLoader() {
 		return new DefaultMustacheTemplateLoader();
 	}
 
+	/**
+	 * Resolve views prefix value.
+	 * Default is to look for "mustache.prefix" property or use {@link #PREFIX} if
+	 * property cannot be resolved.
+	 *
+	 * @return Prefix value.
+	 */
 	protected String getPrefix() {
 		return environment.getProperty("mustache.prefix", PREFIX).trim();
 	}
 
+	/**
+	 * Resolve views suffix value.
+	 * Default is to look for "mustache.suffix" property or use {@link #SUFFIX} if
+	 * property cannot be resolved.
+	 *
+	 * @return Suffix value.
+	 */
 	protected String getSuffix() {
 		return environment.getProperty("mustache.suffix", SUFFIX).trim();
 	}
 
+	/**
+	 * Resolve mustache view resolver order.
+	 * Default is to look for "mustache.order" property or use {@link #ORDER} if
+	 * property cannot be resolved.
+	 *
+	 * @return Order value.
+	 */
 	protected int getOrder() {
 		return parseInt(environment.getProperty("mustache.order", valueOf(ORDER)).trim());
 	}
 
+	/**
+	 * Resolve mustache view resolver cache settings.
+	 * Default is to look for "mustache.cache" property or use {@link #CACHE} if
+	 * property cannot be resolved.
+	 *
+	 * @return Cache settings.
+	 */
 	protected boolean getCache() {
 		return parseBoolean(environment.getProperty("mustache.cache", valueOf(CACHE)).trim());
+	}
+
+	/**
+	 * Resolve view names matchers of mustache view resolver.
+	 * Default is to look for "mustache.viewNames" property or use {@link #VIEW_NAMES} if
+	 * property cannot be resolved.
+	 *
+	 * @return View names patterns.
+	 */
+	protected String[] getViewNames() {
+		String viewNames = environment.getProperty("mustache.viewNames", valueOf(VIEW_NAMES)).trim();
+		String[] names = viewNames.split(",");
+		for (int i = 0; i < names.length; i++) {
+			names[i] = names[i].trim();
+		}
+		return names;
 	}
 }
