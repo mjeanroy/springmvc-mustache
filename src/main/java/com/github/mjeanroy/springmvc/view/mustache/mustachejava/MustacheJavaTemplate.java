0019950d168ac45d1b802de660ea51f1094c2f37
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2014-09-22 13:51:10 +0200

    feat(mustachejava): add mustache.java template

---
+++ src/main/java/com/github/mjeanroy/springmvc/view/mustache/mustachejava/MustacheJavaTemplate.java
@@ -0,0 +1,60 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.springmvc.view.mustache.mustachejava;
+
+import static com.github.mjeanroy.springmvc.view.mustache.commons.PreConditions.notNull;
+
+import java.io.Writer;
+import java.util.Map;
+
+import com.github.mjeanroy.springmvc.view.mustache.MustacheTemplate;
+import com.github.mustachejava.Mustache;
+
+/**
+ * Implementation of {@link com.github.mjeanroy.springmvc.view.mustache.MustacheTemplate} using
+ * mustache.java as real template implementation.
+ */
+public class MustacheJavaTemplate implements MustacheTemplate {
+
+	/**
+	 * Mustache.java template.
+	 * This template will be rendered using mustache.java api.
+	 */
+	private final Mustache mustache;
+
+	/**
+	 * Build new template.
+	 *
+	 * @param mustache Mustache template.
+	 */
+	public MustacheJavaTemplate(Mustache mustache) {
+		this.mustache = notNull(mustache, "Mustache template must not be null");
+	}
+
+	@Override
+	public void execute(Map<String, Object> model, Writer writer) {
+		mustache.execute(writer, model);
+	}
+}
---
+++ src/test/java/com/github/mjeanroy/springmvc/view/mustache/mustachejava/MustacheJavaTemplateTest.java
@@ -0,0 +1,61 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.springmvc.view.mustache.mustachejava;
+
+import static org.mockito.Mockito.*;
+
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.mustachejava.Mustache;
+
+public class MustacheJavaTemplateTest {
+
+	private Mustache mustache;
+
+	private MustacheJavaTemplate mustacheJavaTemplate;
+
+	@Before
+	public void setUp() {
+		mustache = mock(Mustache.class);
+		mustacheJavaTemplate = new MustacheJavaTemplate(mustache);
+	}
+
+	@Test
+	public void it_should_execute_template() throws Exception {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("foo", "bar");
+
+		Writer writer = mock(Writer.class);
+
+		mustacheJavaTemplate.execute(model, writer);
+
+		verify(mustache).execute(writer, model);
+	}
+}
