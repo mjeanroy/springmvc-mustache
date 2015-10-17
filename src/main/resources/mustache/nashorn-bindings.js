282f140dc0aaa4cd2db635abc2b2f4d499cc71bc
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2015-10-17 17:52:27 +0200

    docs: update js comments

--- src/main/resources/mustache/nashorn-bindings.js
+++ src/main/resources/mustache/nashorn-bindings.js
@@ -2,8 +2,8 @@
  * Bindings to use mustache.js with Nashorn.
  */
 
-// Render mustache partials.
 // This function will be called during script execution.
+// This is a simple proxy to mustache implementation.
 function render(template, view, partials) {
   return Mustache.render(template, view, partials);
 }
